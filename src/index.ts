a64edd88088dd0e01d6fbe8121938b324c991c00
afelixj89 <90149052+afelixj89@users.noreply.github.com>
2024-05-30 20:43:57 -0400

    added a home route message for the api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// password dSox37eYkrCJBVV8
 import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
@@ -9,12 +8,18 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
 const mongoURI: string =
   "mongodb+srv://afelixj89:dSox37eYkrCJBVV8@personalfinancetracker.unee92a.mongodb.net/";
 mongoose
   .connect(mongoURI)
   .then(() => console.log("CONNECTED TO MONGO DB!!"))
   .catch((err) => console.error("Failed to Connect to Mongo DB", err));
+
+// Define the home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Financial Records API');
+});
 
 app.use("/financial-record", financialRecordRouter);
 
